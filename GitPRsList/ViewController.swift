c90134d85688cdb1c2c3fb41fe5f35c69a08bf49
Abhijeet Choudhary <abhijeetchoudhary@192.168.1.3>
2022-09-26 20:42:35 +0530

    1. Project Created 2. ReadMe.md added 3. .gitignore added

---
+++ GitPRsList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitPRsList
+//
+//  Created by Abhijeet Choudhary on 26/09/22.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
---
+++ README.md
@@ -0,0 +1,3 @@
+GitPrsList
+
+# Using Github APIs to fetch list of Public Pull Requests in an iOS application.
